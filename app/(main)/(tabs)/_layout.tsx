895ecfdb55887bc6d1b3e409c3656e2faf0e0dc2
yeeeeeeeop <yeop0916@gmail.com>
2024-09-20 18:06:07 +0900

    [#5] feat: 바텀 탭 커스텀 적용(스타일 적용 X)

--- app/(main)/(tabs)/_layout.tsx
+++ app/(main)/(tabs)/_layout.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Tabs, usePathname} from 'expo-router';
+import TabBar from '@/src/common/TabBar';
 
 export default function TabLayout() {
   const pathname = usePathname();
@@ -7,11 +8,14 @@ export default function TabLayout() {
   const displayTabBar = pathname === '/' ? 'flex' : 'none';
 
   return (
-    <Tabs screenOptions={{headerShown: false, tabBarStyle: {display: displayTabBar}}}>
-      <Tabs.Screen name='(home)' />
-      <Tabs.Screen name='(care)' />
-      <Tabs.Screen name='(community)' />
-      <Tabs.Screen name='(market)' />
+    <Tabs
+      screenOptions={{headerShown: false, tabBarStyle: {display: displayTabBar}}}
+      tabBar={props => <TabBar {...props} />}
+    >
+      <Tabs.Screen name='(home)' options={{tabBarLabel: '홈'}} />
+      <Tabs.Screen name='(care)' options={{tabBarLabel: '배변일지'}} />
+      <Tabs.Screen name='(community)' options={{tabBarLabel: '커뮤니티'}} />
+      <Tabs.Screen name='(market)' options={{tabBarLabel: '대장간'}} />
     </Tabs>
   );
 }
