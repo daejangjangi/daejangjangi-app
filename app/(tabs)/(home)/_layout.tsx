e82f7bec2eb8c5f4f34e6daa080c73fd4404c700
yeeeeeeeop <yeop0916@gmail.com>
2024-09-20 16:43:19 +0900

    [#5] feat: 커스텀 헤더 적용

--- app/(tabs)/(home)/_layout.tsx
+++ app/(tabs)/(home)/_layout.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
 import {Stack} from 'expo-router';
+import CommonHeader from '@/src/common/headers';
+
+const homeRouteMap = {
+  cartoon: '대장툰',
+  'card-news': '카드뉴스',
+  'news-letter': '상품추천 뉴스레터',
+  reels: '레시피 릴스',
+};
 
 export default function HomeLayout() {
   return (
-    <Stack>
+    <Stack
+      screenOptions={({route}) => ({
+        header: () => <CommonHeader routeName={route.name} routeMap={homeRouteMap} title='홈' />,
+      })}
+    >
       <Stack.Screen name='index' />
       <Stack.Screen name='cartoon' />
       <Stack.Screen name='card-news' />
--- app/(tabs)/(market)/_layout.tsx
+++ app/(tabs)/(market)/_layout.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
 import {Stack} from 'expo-router';
+import CommonHeader from '@/src/common/headers';
+
+const marketRouteMap = {
+  favorite: '관심 상품',
+  search: '검색',
+};
 
 export default function MarketLayout() {
   return (
-    <Stack>
+    <Stack
+      screenOptions={{
+        header: ({route}) => (
+          <CommonHeader routeMap={marketRouteMap} routeName={route.name} title='대장간' />
+        ),
+      }}
+    >
       <Stack.Screen name='index' />
       <Stack.Screen name='favorite' />
       <Stack.Screen name='search' />
