195ad7ab16c2febbc3d2f004c47b5328213f3eba
yeeeeeeeop <yeop0916@gmail.com>
2024-09-20 11:14:42 +0900

    [#5] feat: 필요 페이지 기본 screen 컴포넌트 생성

---
+++ src/features/community/CommunityScreen.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+export default function CommunityScreen() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <Text>CommunityScreen</Text>
+    </View>
+  );
+}
---
+++ src/features/market/MarketScreen.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+export default function MarketScreen() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <Text>MarketScreen</Text>
+    </View>
+  );
+}
