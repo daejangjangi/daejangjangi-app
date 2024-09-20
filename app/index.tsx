5f6497c15d77ab19f924cecf8177f374931027b4
gonu <mambomul@naver.com>
2024-09-20 20:35:22 +0900

    [#6] test: 공통 컴포넌트 테스트

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,25 @@
-import { Text, View } from "react-native";
+import React from 'react';
+import CommonTextField from '@/src/common/CommonTextField';
+import {Text, View} from 'react-native';
+import CommonSearchBar from '@/src/common/CommonSearchBar';
+import CommonModal from '@/src/common/CommonModal';
+import CommonCheckBox from '@/src/common/CommonCheckBox';
 
 export default function Index() {
   return (
     <View
       style={{
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
+        justifyContent: 'center',
+        alignItems: 'center',
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <CommonModal subject='관심질환'>
+        <CommonCheckBox values={['변비', '치질', '과민성 장 증후군']} />
+      </CommonModal>
+      {/* <CommonTextField placeholder='텍스트 입력' />
+      <CommonSearchBar placeholder='검색' /> */}
+      {/* <Text>Edit app/index.tsx to edit this screen.</Text> */}
     </View>
   );
 }
