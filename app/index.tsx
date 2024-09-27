6c3b366b3e6f5dcbf743781bcbd9c136a865816c
yeeeeeeeop <yeop0916@gmail.com>
2024-09-27 16:18:21 +0900

    [#12] feat: theme 적용

--- app/index.tsx
+++ app/index.tsx
@@ -1,20 +1,32 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {View} from 'react-native';
 import {Link} from 'expo-router';
+import styled, {ThemeProvider} from 'styled-components/native';
+
+import {theme} from '@/src/styles/theme';
+import {AppText} from '@/src/common/AppComponents';
+
+const StyledText = styled(AppText)`
+  font-size: 32px;
+  margin-bottom: 20px;
+  color: ${props => props.theme.colors.main};
+`;
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-      <Link href='/signin'>SignIn</Link>
-      <Link href='/signup'>SignUp</Link>
-      <Link href='/(tabs)'>tabs</Link>
-    </View>
+    <ThemeProvider theme={theme}>
+      <View
+        style={{
+          flex: 1,
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <StyledText>프리텐다드</StyledText>
+        <Link href='/signin'>SignIn</Link>
+        <Link href='/signup'>SignUp</Link>
+        <Link href='/(tabs)'>tabs</Link>
+      </View>
+    </ThemeProvider>
   );
 }
