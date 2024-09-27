cea8c579f55a0578a5b2c46a206ea0c985492d12
yeeeeeeeop <yeop0916@gmail.com>
2024-09-27 17:24:40 +0900

    [#12] feat: 공용 폰트 타입 설정 및 사용예시 작성

--- src/common/AppComponents.tsx
+++ src/common/AppComponents.tsx
@@ -1,23 +1,36 @@
 import React from 'react';
-import {Text, TextProps, StyleSheet, TextInput} from 'react-native';
+import {TextProps} from 'react-native';
+import {FontsTypes} from '@/src/styles/theme';
+import styled from 'styled-components/native';
 
-const styles = StyleSheet.create({
-  defaultText: {
-    fontFamily: 'PretendardVariable', // 기본 폰트 설정
-  },
-});
+interface CustomTextProps {
+  // eslint-disable-next-line react/require-default-props
+  textType?: keyof FontsTypes | undefined;
+}
+
+const S = {
+  CustomText: styled.Text<CustomTextProps>`
+    font-family: 'PretendardVariable';
+    ${props => (props.textType ? props.theme.fonts[props.textType] : '')};
+  `,
+
+  CustomTextInput: styled.TextInput`
+    font-family: 'PretendardVariable';
+    ${props => (props.textType ? props.theme.fonts[props.textType] : '')};
+  `,
+};
 
 // eslint-disable-next-line react/function-component-definition
-export const AppText: React.FC<TextProps> = props => (
+export const AppText: React.FC<TextProps & CustomTextProps> = props => (
   // eslint-disable-next-line react/destructuring-assignment,react/jsx-props-no-spreading
-  <Text {...props} style={[styles.defaultText, props.style]}>
+  <S.CustomText {...props}>
     {/* eslint-disable-next-line react/destructuring-assignment */}
     {props.children}
-  </Text>
+  </S.CustomText>
 );
 
 // eslint-disable-next-line react/function-component-definition
 export const AppTextInput: React.FC<TextProps> = props => (
   // eslint-disable-next-line react/jsx-props-no-spreading,react/destructuring-assignment
-  <TextInput {...props} style={[styles.defaultText, props.style]} />
+  <S.CustomTextInput {...props} />
 );
