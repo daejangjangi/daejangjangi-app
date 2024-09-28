e90c21caf3281a6cb5f81853231d109b7d9185cd
yeeeeeeeop <yeop0916@gmail.com>
2024-09-28 14:43:15 +0900

    [#13] add: icon 추가 - IcHome.svg - IcCalendar.svg - IcCommunity.svg - IcMarket.svg - IcUser.svg

---
+++ assets/images/icons/index.ts
@@ -0,0 +1,5 @@
+export {default as IcHome} from './IcHome.svg';
+export {default as IcCommunity} from './IcCommunity.svg';
+export {default as IcUser} from './IcUser.svg';
+export {default as IcCalendar} from './IcCalendar.svg';
+export {default as IcMarket} from './IcMarket.svg';
