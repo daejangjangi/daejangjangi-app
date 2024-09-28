510d117b4cac0d9943aae2a1bdddff0b6d1bcf86
yeeeeeeeop <yeop0916@gmail.com>
2024-09-28 15:53:54 +0900

    [#13] add: IcNext 추가

--- assets/images/icons/index.ts
+++ assets/images/icons/index.ts
@@ -3,3 +3,4 @@ export {default as IcCommunity} from './IcCommunity.svg';
 export {default as IcUser} from './IcUser.svg';
 export {default as IcCalendar} from './IcCalendar.svg';
 export {default as IcMarket} from './IcMarket.svg';
+export {default as IcNext} from './IcNext.svg';
