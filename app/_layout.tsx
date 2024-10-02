670951bf0feaa7f218bd9ad0b37673754af79fa6
yeeeeeeeop <yeop0916@gmail.com>
2024-10-02 22:33:45 +0900

    [#23] fix: ThemeProvider 위치 변경

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import {Stack} from 'expo-router';
 import 'expo-dev-client';
+import {theme} from '@/src/styles/theme';
+import {ThemeProvider} from 'styled-components/native';
 
 export default function RootLayout() {
   return (
-    <Stack screenOptions={{headerShown: false}}>
-      <Stack.Screen name='index' />
-      <Stack.Screen name='signin' />
-      <Stack.Screen name='signup' />
-      <Stack.Screen name='(main)' />
-    </Stack>
+    <ThemeProvider theme={theme}>
+      <Stack screenOptions={{headerShown: false}}>
+        <Stack.Screen name='index' />
+        <Stack.Screen name='signin' />
+        <Stack.Screen name='signup' />
+        <Stack.Screen name='(main)' />
+      </Stack>
+    </ThemeProvider>
   );
 }
