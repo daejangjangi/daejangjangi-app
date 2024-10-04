0e5062f1496c42635cd7fcd9906f11382c370461
yeeeeeeeop <yeop0916@gmail.com>
2024-10-04 15:37:00 +0900

    [#19] feat: 로그인 화면 구현

--- src/features/signin/SignInScreen.tsx
+++ src/features/signin/SignInScreen.tsx
@@ -1,16 +1,40 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import SignInHeader from '@/src/features/signin/components/SignInHeader';
+import SignInForm from '@/src/features/signin/components/SignInForm';
+import SocialLoginForm from '@/src/features/signin/components/SocialLoginForm';
+import styled from 'styled-components/native';
+
+const S = {
+  Container: styled.View`
+    flex-direction: column;
+    padding: 20px;
+    flex: 1;
+  `,
+
+  NormalForm: styled.View`
+    margin-top: 80px;
+    flex: 1;
+    justify-content: center;
+    align-items: center;
+    gap: 60px;
+  `,
+
+  SocialForm: styled.View`
+    bottom: 0;
+  `,
+};
 
 export default function SignInScreen() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}
-    >
-      <Text>SignInScreen</Text>
-    </View>
+    <S.Container>
+      <S.NormalForm>
+        <SignInHeader />
+        <SignInForm />
+      </S.NormalForm>
+
+      <S.SocialForm>
+        <SocialLoginForm />
+      </S.SocialForm>
+    </S.Container>
   );
 }
