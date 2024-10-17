bf3f597c329bf11071114e2417a34426510430e1
yeeeeeeeop <yeop0916@gmail.com>
2024-10-17 22:21:31 +0900

    [#30] feat: 자주 묻는 질문 UI 구현

--- src/features/mypage/faq/FaqScreen.tsx
+++ src/features/mypage/faq/FaqScreen.tsx
@@ -1,16 +1,27 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import styled from 'styled-components/native';
+import FaqAccordion from '@/src/features/mypage/faq/FaqAccordion';
+
+const S = {
+  Container: styled.ScrollView`
+    flex: 1;
+    background-color: #fff;
+    padding: 20px;
+  `,
+};
 
 export default function FaqScreen() {
+  /**
+   * @Todo: 내용 불러오기 로직 구현
+   */
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}
-    >
-      <Text>FaqScreen</Text>
-    </View>
+    <S.Container>
+      <FaqAccordion title='비밀번호를 잊어버렸어요.' body='비밀번호를 잊어버렸어요.' />
+      <FaqAccordion
+        title='추가 등록 회원도 맞춤 상품 추천 받을 수 있나요?'
+        body='추가 등록 회원도 맞춤 상품 추천 받을 수 있나요?'
+      />
+    </S.Container>
   );
 }
