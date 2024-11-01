1ce4d399afba4ad6b9a08babecc742a502e44f50
yeeeeeeeop <yeop0916@gmail.com>
2024-11-01 14:39:49 +0900

    [#41] feat: CommonCheckBox 초기상태 추가

--- src/common/CommonCheckBox.tsx
+++ src/common/CommonCheckBox.tsx
@@ -1,10 +1,11 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 import styled from 'styled-components/native';
 import {Image, Pressable} from 'react-native';
 import {AppText} from '@/src/common/AppComponents';
 
 type CheckBoxProps = {
   values: string[];
+  initialCheckedState?: boolean[];
   onChange: (value: boolean[]) => void;
 };
 
@@ -27,8 +28,24 @@ const S = {
   `,
 };
 
-export default function CommonCheckBox({values, onChange}: CheckBoxProps) {
-  const [selectedItems, setSelectedItems] = useState<boolean[]>(values.map(() => false));
+export default function CommonCheckBox({values, initialCheckedState, onChange}: CheckBoxProps) {
+  const [selectedItems, setSelectedItems] = useState<boolean[]>(
+    initialCheckedState || values.map(() => false),
+  );
+
+  // 이전 initialCheckedState를 저장
+  const prevInitialState = useRef(initialCheckedState);
+
+  useEffect(() => {
+    // initialCheckedState가 변경되었고, 이전 값과 다를 때만 업데이트
+    if (
+      initialCheckedState &&
+      JSON.stringify(prevInitialState.current) !== JSON.stringify(initialCheckedState)
+    ) {
+      setSelectedItems(initialCheckedState);
+      prevInitialState.current = initialCheckedState;
+    }
+  }, [initialCheckedState]);
 
   const onPressHandler = (index: number) => {
     const newSelectedItems = [...selectedItems];
@@ -41,13 +58,13 @@ export default function CommonCheckBox({values, onChange}: CheckBoxProps) {
 
   return (
     <S.CheckBoxContainer>
-      {values.map((text, index) => (
-        <S.Item key={index}>
+      {values.map(text => (
+        <S.Item key={`checkbox-${text}`}>
           <S.Text>{text}</S.Text>
-          <Pressable onPress={() => onPressHandler(index)}>
+          <Pressable onPress={() => onPressHandler(values.indexOf(text))}>
             <Image
               source={
-                selectedItems[index]
+                selectedItems[values.indexOf(text)]
                   ? require('assets/images/radio_button_checked.png')
                   : require('assets/images/radio_button.png')
               }
