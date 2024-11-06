aec5f69f2a67bd1c0a599f1fa5956fdd31865690
yeeeeeeeop <yeop0916@gmail.com>
2024-11-06 19:27:49 +0900

    [#43] feat: 카드뉴스 api, 쿼리 구현

---
+++ src/api/cardnews.ts
@@ -0,0 +1,29 @@
+import httpInstance from './http';
+import {CardNews, CardNewsListItem} from './types/cardnews.type';
+
+// 목록 조회
+async function getCardNewsList() {
+  const response = await httpInstance.get<CardNewsListItem[]>('/v1/cardnews');
+
+  return response;
+}
+
+// 상세 조회
+async function getCardNewsDetail(id: number) {
+  const response = await httpInstance.get<CardNews>(`/v1/cardnews/${id}`);
+
+  return response;
+}
+
+// 최신 조회
+async function getCardNewsLatest() {
+  const response = await httpInstance.get<CardNews>('/v1/cardnews/latest');
+
+  return response;
+}
+
+export const CardNewsApi = {
+  getCardNewsList,
+  getCardNewsDetail,
+  getCardNewsLatest,
+};
---
+++ src/api/types/cardnews.type.ts
@@ -0,0 +1,11 @@
+export interface CardNews {
+  title: string;
+  createdAt: string;
+  newImages: string[];
+}
+
+export interface CardNewsListItem {
+  id: string;
+  title: string;
+  profile: string;
+}
