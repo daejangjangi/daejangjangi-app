f8ac6259f98d451c76ac9b358a9df8e66f8010c4
yeeeeeeeop <yeop0916@gmail.com>
2024-11-09 16:35:19 +0900

    [#44] fix: enum 내 값 변경

--- src/api/types/qna.type.ts
+++ src/api/types/qna.type.ts
@@ -1,11 +1,11 @@
 export enum QnaCategory {
-  '기능제안',
-  '문의사항',
+  '기능제안' = '기능제안',
+  '문의사항' = '문의사항',
 }
 
 export enum QnaStatus {
-  '답변대기',
-  '답변완료',
+  '답변대기' = '답변대기',
+  '답변완료' = '답변완료',
 }
 
 export interface Question {
