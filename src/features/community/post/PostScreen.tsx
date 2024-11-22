2215975335639d97b36caf1ad42109d4ff63b944
yeeeeeeeop <yeop0916@gmail.com>
2024-11-22 18:40:29 +0900

    [#0] fix: 댓글 삭제 위치 변경

--- src/features/community/post/PostScreen.tsx
+++ src/features/community/post/PostScreen.tsx
@@ -169,8 +169,8 @@ const S = {
 
   MenuModal: styled.View`
     position: absolute;
-    right: 20px;
-    top: 50px;
+    right: 8px;
+    top: 40px;
     background-color: white;
     border-radius: 8px;
     elevation: 5;
