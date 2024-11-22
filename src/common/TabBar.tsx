57c7759f9048eedc5c7674739123093aeaa14ab4
yeeeeeeeop <yeop0916@gmail.com>
2024-11-22 10:15:23 +0900

    [#70] feat: 대장간 탭 복구

--- src/common/TabBar.tsx
+++ src/common/TabBar.tsx
@@ -40,7 +40,6 @@ export default function TabBar({state, descriptors, navigation}: BottomTabBarPro
     <S.Container>
       {state.routes
         .filter(route => route.name !== 'index')
-        .filter(route => route.name !== 'market')
         .map((route, index) => {
           const {options} = descriptors[route.key];
           const label =
