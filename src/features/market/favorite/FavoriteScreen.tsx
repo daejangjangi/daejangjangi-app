eb1a3f900c4289b70d91f8e5f7d20d979ec0f435
yeeeeeeeop <yeop0916@gmail.com>
2024-11-26 10:33:33 +0900

    [#70] feat: 관심상품 UI 구현

--- src/features/market/favorite/FavoriteScreen.tsx
+++ src/features/market/favorite/FavoriteScreen.tsx
@@ -1,16 +1,31 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import styled from 'styled-components/native';
+import ProductItem from '../components/ProductItem';
+
+const S = {
+  Container: styled.View`
+    flex: 1;
+    background-color: #fff;
+    padding: 20px;
+  `,
+
+  ProductItemList: styled.View`
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: space-between;
+  `,
+};
 
 export default function FavoriteScreen() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}
-    >
-      <Text>FavoriteScreen</Text>
-    </View>
+    <S.Container>
+      <S.ProductItemList>
+        <ProductItem />
+        <ProductItem />
+        <ProductItem />
+        <ProductItem />
+        <ProductItem />
+      </S.ProductItemList>
+    </S.Container>
   );
 }
